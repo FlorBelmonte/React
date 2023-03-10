8b324761f7dccb2206bcb4c5a06c1f6fa93c32eb
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-03-10 19:27:45 -0300

    Entrega del ejercicio 5 Clase 2/3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 // import FeedbackForm from "./Ejercicio3";
 // import FeedbackForm2 from "./Ejercicio4";
 import LightSwitch from "./Desafio1";
-// import UsandoPokeApi from "./EjercicioUseEffect";
+import UsandoPokeApi from "./EjercicioUseEffect";
+import GroceryList from "./Desafio5";
 
 
 function App() {
@@ -14,9 +15,10 @@ function App() {
       {/* <Gallery/>
       <Form/>
     <FeedbackForm/>
-    <FeedbackForm2/>
-    <UsandoPokeApi/> */}
+    <FeedbackForm2/> */}
+    <UsandoPokeApi/>
     <LightSwitch/>
+    <GroceryList/>
     </div>
   );
 }
